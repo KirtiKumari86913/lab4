6c83e1a2f1b1a4b6dc5015a55fc3b29d6a3afce8
KirtiKumari86913 <2021wb86913@wilp.bits-pilani.ac.in>
2025-07-13 20:44:39 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,9 @@
+package com.example;
+public class App {
+  public static void main(String[] args) {
+    system.out.println("Hello, Maven + Jenkins World!");
+  }
+  public static String greet() {
+    return "Hello from App!";
+  }
+}
