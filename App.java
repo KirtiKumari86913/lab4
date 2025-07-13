bae70e62eba0a5789b65c570f081dc75b7ce663c
KirtiKumari86913 <2021wb86913@wilp.bits-pilani.ac.in>
2025-07-13 20:45:27 +0530

    Add files via upload

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
-package com.example;
-public class App {
-  public static void main(String[] args) {
-    system.out.println("Hello, Maven + Jenkins World!");
-  }
-  public static String greet() {
-    return "Hello from App!";
-  }
+package com.deveops.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
